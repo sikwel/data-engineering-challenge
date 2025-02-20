9b5d1fc95d9a12ba205612c8343e675f1b879138
Michi Schwarz <michischwarz@posteo.de>
2025-02-20 14:06:40 +0100

    util db_filler querying usefull timeframe for analysis

--- utils/clean_and_load_all_data.sh
+++ utils/clean_and_load_all_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # This calls our data extraction and fills it up with all the data so we can do some stuff with it
 
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'full' --lat 34.31295 --long -78.16111 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 42.21561 --long -79.83422 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 43.2342 --long -86.2506 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 40.19896 --long -79.52087 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 38.88067 --long -76.98998 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 60.10867 --long -113.64258 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'full' --lat 34.31295 --long -78.16111 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 42.21561 --long -79.83422 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 43.2342 --long -86.2506 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 40.19896 --long -79.52087 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 38.88067 --long -76.98998 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 60.10867 --long -113.64258 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
 python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 46.0 --long 2.0
 python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat 51.5 --long 10.5
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat -25.0 --long 135.0 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
-python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat  54.75844 --long -2.69531 --start_date '2025-01-01' --end_date '2025-01-10' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat -25.0 --long 135.0 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
+python "M:\Code\sikwel_codingchallenge\data-engineering-challenge\extract_meteo\main.py" --env 'dev' --load_type 'incremental' --lat  54.75844 --long -2.69531 --start_date '2017-07-01' --end_date '2020-05-31' --tz 'GMT+1'
