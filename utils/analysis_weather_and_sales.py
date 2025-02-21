25dbbaf65e16e2edd2ed754e7772934438556162
Michi Schwarz <michischwarz@posteo.de>
2025-02-21 12:58:06 +0100

    dummy weather_sales_analysis

---
+++ utils/analysis_weather_and_sales.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import seaborn as sns
+import duckdb
+
+duck_db_conn_str = '../duckdb/dev_db.duckdb'
+con = duckdb.connect(duck_db_conn_str)
+sql_query_tab = """
+    SELECT
+        *
+    FROM main.weather_and_sales
+"""
+df = con.query(sql_query_tab).df()
+
+
+
+# Number of orders related to the average temperature?
+sns.lmplot(
+    data=df, x="temperature_avg", y="cnt_orders"
+    ,col="sales_territory_key"
+)
+
+# Average Sales per day related to the average temperature?
+sns.lmplot(
+    data=df, x="temperature_avg", y="avg_sales_dollar"
+    ,col="sales_territory_key"
+)
+
+# Number of orders related to the average rain?
+sns.lmplot(
+    data=df, x="rain_avg", y="cnt_orders"
+    ,col="sales_territory_key"
+)
+
+# Average Sales per day related to the average rain?
+sns.lmplot(
+    data=df, x="temperature_avg", y="avg_sales_dollar"
+    ,col="sales_territory_key"
+)
